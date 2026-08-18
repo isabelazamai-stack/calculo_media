2c99f9a7c7b9ebe3c4ec58c69641f4386b9f144c
isabela <isabela.zamai@edu.senai.br>
2026-08-18 14:18:12 -0300

    ADD

--- calculo_media.py
+++ calculo_media.py
@@ -9,30 +9,49 @@ def main(pagina:ft.Page):
                      size = 40,
                      font_family = "Baskerville Old Face",
                      color= "#924A10")
-
-    botao = ft.FloatingActionButton(icon = ft.Icons.ADD,
+    
+    lista_notas = []
+    
+    def adicionar_nota():
+        lista_notas.append(ft.TextField(label="NOTA",
+                                        filled=True,
+                                        bgcolor="#ffacac",
+                                        border_color="#ffffff"))
+    
+    botao = ft.FloatingActionButton(icon = ft.Icon(icon=ft.CupertinoIcons.ADD_CIRCLED,
+                                                   color="#A73763"),
                                   bgcolor="#f8d1d1",
                                   foreground_color="#FF3D3D",
-                                  hover_color="#ff9595"
+                                  hover_color="#ff9595",
+                                  on_click=adicionar_nota
                                   )
+    
+    coluna_notas = ft.Column(controls=lista_notas,
+                             expand=True,
+                             wrap=True,
+                             scroll=ft.ScrollMode.ADAPTIVE,
+                             horizontal_alignment="center")
 
     botao_resultado = ft.FilledTonalButton(content="Calcular Média",
-                                           bgcolor="#f8d1d1",
+                                           bgcolor="#f8d1d1"
                                            )
 
     campo_resultado = ft.TextField(value = 0,
                                    label="Resultado",
                                    read_only=True,
-                                   text_align="center")
+                                   text_align="center",
+                                   bgcolor="#f8d1d1")
 
     
 
     linha_resultado = ft.Row(controls=[botao_resultado,
-                                       campo_resultado])
+                                       campo_resultado],
+                                       alignment="center")
 
     pagina.controls = [titulo,
                        botao,
-                       linha_resultado,
+                       coluna_notas,
+                       linha_resultado
                        ]
 
     pagina.update()
