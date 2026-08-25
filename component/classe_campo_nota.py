92cc4c72f845791c3ea1566efb37cd8993a041da
Isabela <isabela.zamai@edu.senai.br>
2026-08-25 11:26:08 -0300

    add

--- component/classe_campo_nota.py
+++ component/classe_campo_nota.py
@@ -4,22 +4,33 @@ class Campo_nota(ft.Row):
     def __init__(self):
         super().__init__()
 
-        caixa_texto = ft.TextField(label="NOTA",
+        self.caixa_texto = ft.TextField(label="NOTA",
                                    filled=True,
                                    bgcolor="#ffe3e3")
 
-        caixa_selecao = ft.Checkbox()
+        self.caixa_selecao = ft.Checkbox(on_change=self.alteral_cor)
 
-        caixa_bonita = ft.Container(content=ft.Row(controls=[caixa_selecao,
-                                                             caixa_texto],
+        self.caixa_bonita = ft.Container(content=ft.Row(controls=[self.caixa_selecao,
+                                                             self.caixa_texto],
                                                              ),
-                                                             bgcolor="#ffffff",
+                                                             bgcolor="#fff8e3",
                                                              border=ft.Border.all(width=1,
                                                                                   color="#ffffff"),
                                                             border_radius=10,
-                                                            padding=5)
+                                                            padding=5,
+                                                            animate=True)
 
         self.controls = [
-            caixa_bonita
+            self.caixa_bonita
         ]
+
+    def alteral_cor(self):
+        if self.caixa_selecao.value == True:
+            self.caixa_bonita.bgcolor="#B17C56"
+        else:
+            self.caixa_bonita.bgcolor = "#ffe3e3"
+
+    @property
+    def value(self):
+        return self.caixa_texto.value
 
